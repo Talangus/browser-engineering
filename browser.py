9a95c8d980cdc9ef9be7808eaec643622d169c8d
Tal <tal.langus@ibm.com>
2025-06-06 12:18:51 +0300

    exec1-4

--- browser.py
+++ browser.py
@@ -1,14 +1,25 @@
 import sys
 
+import utils
 from url import URL
+
 
 def show(body):
     in_tag = False
+    entity_buffer = ""
     for c in body:
         if c == "<":
             in_tag = True
         elif c == ">":
             in_tag = False
+        elif c == "&" and not in_tag:
+            entity_buffer = "&"
+        elif entity_buffer:
+            entity_buffer += c
+            if c == ";":
+                entity = "".join(entity_buffer)
+                print(utils.html_unescape(entity), end="")
+                entity_buffer = ""
         elif not in_tag:
             print(c, end="")
 
@@ -21,8 +32,8 @@ def get_url_arg():
         return sys.argv[1]
     else:
         # return URL.DEFAULT_FILE_PATH
-        return 'data:text/html;base64,PGgxPkhlbGxvIFdvcmxkPC9oMT4='
-        # return 'https://browser.engineering/http.html'
+        # return 'data:text/html;base64,PGgxPkhlbGxvIFdvcmxkPC9oMT4='
+        return 'https://browser.engineering/http.html'
 
 if __name__ == "__main__":
     
