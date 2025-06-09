462bb2072a6a41f1462201e59f4183ae802e7d25
Tal <tal.langus@ibm.com>
2025-06-09 09:34:15 +0300

    chapter 3 template

---
+++ layout.py
@@ -0,0 +1,95 @@
+import tkinter.font
+import utils
+from config import WIDTH, HEIGHT, HSTEP, VSTEP
+
+# --- Token classes ---
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return f"Text({self.text!r})"
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __repr__(self):
+        return f"Tag({self.tag!r})"
+
+# --- Font cache ---
+FONTS = {}
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        FONTS[key] = font
+    return FONTS[key]
+
+# --- Layout ---
+class Layout:
+    def __init__(self, tokens, width=WIDTH, hstep=HSTEP, vstep=VSTEP):
+        self.tokens = tokens
+        self.display_list = []
+
+        self.cursor_x = hstep
+        self.cursor_y = vstep
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+
+        self.line = []
+        self.width = width
+        self.hstep = hstep
+        self.vstep = vstep
+
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += self.vstep
+
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+        if self.cursor_x + w > self.width - self.hstep:
+            self.flush()
+        self.line.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_x = self.hstep
+        self.line = []
